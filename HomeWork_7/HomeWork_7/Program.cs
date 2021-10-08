a5d83551ef117c0853bed35d5732116ace523bc3
ChivononGeko <74726135+ChivononGeko@users.noreply.github.com>
2021-10-08 22:41:54 +0600

    Add files via upload

---
+++ HomeWork_7/HomeWork_7/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace HomeWork_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"data.txt";
+            Repository rep = new Repository(path);
+
+            Console.WriteLine("Здравствуйте ! Для демонстрации существующих  записей нажмите 'Enter', для добавления новой записи " +
+                "нажмите 'Spacebar', для удаления записи нажмите 'Delete', для редактирования записи нажмите 'Backspace', для сортировки по определенному полю нажмите 'Q'," +
+                " для вывода данных по диапозону дат введите 'W'...");
+            rep.Load();
+            do
+            { 
+            switch (Console.ReadKey().Key)
+            {                          
+                case ConsoleKey.Enter:                  //Печать в консоль
+                       rep.PrintToConsole();
+                   break;
+                case ConsoleKey.Spacebar:               //Добавление
+                       rep.Add(rep.FIlling());
+                       rep.UnLoad();
+                       rep.PrintToConsole();
+                   break;
+                case ConsoleKey.Delete:                 //Удаление
+                        Console.WriteLine("Введите имя по которому вы хотите удалить запись...");
+                        string label = Console.ReadLine();
+                        rep.Remove(label);                       
+                        rep.UnLoad();
+                        rep.PrintToConsole();
+                        break;
+                case ConsoleKey.Backspace:              //Редактирование
+                        Console.WriteLine("Введите имя по которому вы хотите редактировать запись...");
+                        label = Console.ReadLine();                       
+                        if (rep.Edit(label))
+                        { Console.WriteLine("Успешное редактиование..."); }
+                        else
+                        { Console.WriteLine("Такой записи нет..."); }
+                        rep.UnLoad();
+                        rep.PrintToConsole();
+                        break; 
+                    case ConsoleKey.Q:                  //Упорядочивание
+                        Console.WriteLine("Введите номер,по какому полю нужно упорядочить:\n1)По имени\n2)По фамилии\n3)По возрасту");
+                        string n = Console.ReadLine();
+                        rep.Sort(n);
+                        rep.PrintToConsole();
+                   break;
+                    case ConsoleKey.W:                 //Вывод по диапозону дат
+                        Console.WriteLine("Введите начальную и конечную даты в формате XX.XX.XXXX");
+                        DateTime a = DateTime.Parse(Console.ReadLine());
+                        DateTime b = DateTime.Parse(Console.ReadLine());
+                        rep.PrintSort(a, b);
+                        rep.PrintToConsole();
+                        break;
+                    default:
+                    break;
+            }
+            } while (Console.ReadKey().Key != ConsoleKey.Tab);
+        }
+    }
+}
---
+++ HomeWork_7/HomeWork_7/Repository.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HomeWork_7
+{
+    class Repository
+    {
+        /// <summary>
+        /// Массив для хранения данных о записях
+        /// </summary>
+        private Field[] fields;
+        /// <summary>
+        /// Путь к файлу
+        /// </summary>
+        private string path;
+        /// <summary>
+        /// Переменная, аоказывающая количество записей
+        /// </summary>
+        int index;
+        /// <summary>
+        /// Массив для заголовок
+        /// </summary>
+        string[] titles;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="Path"></param>
+        public Repository(string Path)
+        {
+            this.path = Path;
+            this.index = 0;
+            this.titles = new string[5];
+            this.fields = new Field[2];
+        }
+        /// <summary>
+        /// Метод для расширения массива
+        /// </summary>
+        /// <param name="Flag"></param>
+        private void Resize(bool Flag)
+        {
+            if (Flag)
+            {
+                Array.Resize(ref this.fields, this.fields.Length * 2);
+            }
+        }
+        /// <summary>
+        /// Метод для создания новых записей
+        /// </summary>
+        /// <returns></returns>
+        public Field FIlling()
+        {
+            Console.WriteLine("Введите имя..."); string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию..."); string lastname = Console.ReadLine();
+            Console.WriteLine("Введите возраст..."); int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите запись..."); string record = Console.ReadLine();
+            return new Field(name, lastname, age, record);
+        }
+        /// <summary>
+        /// Метод, для добавления записей в массив
+        /// </summary>
+        /// <param name="field">Запись</param>
+        public void Add(Field field)
+        {
+            this.Resize(index >= this.fields.Length);
+            this.fields[index] = field;
+            this.index++;
+        }
+        /// <summary>
+        /// Поиск индекса записи по полю
+        /// </summary>
+        /// <param name="label"></param>
+        public void Remove(string label)
+        {
+            bool flag = false;
+            for (int i = 0; i < this.index; i++)
+            {
+                if (this.fields[i].FirstName.Trim() == label)
+                {
+                    RemByIndex(i--);
+                    flag = true;
+                }
+
+                else if (this.fields[i].LastName.Trim() == label)
+                {
+                    RemByIndex(i--);
+                    flag = true;
+                }
+
+                else if (this.fields[i].Record.Trim() == label)
+                {
+                    RemByIndex(i--);
+                    flag = true;
+                }
+                else if (int.TryParse(label, out int result)  && this.fields[i].Age == result)
+                {
+                    RemByIndex(i--);
+                    flag = true;
+                }
+            }
+            if (flag)
+            { Console.WriteLine("Успешное удаление..."); }
+            else
+            { Console.WriteLine("Такой записи нет..."); }
+        }
+        /// <summary>
+        /// Удаления записи
+        /// </summary>
+        /// <param name="RemoveIndex">Индекс записи для удаления</param>
+        public void RemByIndex(int RemoveIndex)
+        {
+            Field[] newfields = new Field[this.fields.Length - 1];
+            index--;
+            for (int i = 0; i < RemoveIndex; i++)
+            {
+                newfields[i] = this.fields[i];
+            }
+            for (int i = RemoveIndex; i < index; i++)
+            {
+                newfields[i] = this.fields[i + 1];
+            }
+            this.fields = newfields;
+        }
+        /// <summary>
+        /// Редактирвоание
+        /// </summary>
+        /// <param name="label">Значение, по которому надо редактировать запись</param>
+        public bool Edit(string label)
+        {
+            bool flag = false;
+            for (int i = 0; i < this.index; i++)
+            {
+                if (this.fields[i].FirstName.Trim() == label)
+                {
+                    this.fields[i--] = FIlling();
+                    flag = true;
+                }
+
+                else if (this.fields[i].LastName.Trim() == label)
+                {
+                    this.fields[i--] = FIlling();
+                    flag = true;
+                }
+
+                else if (this.fields[i].Record.Trim() == label)
+                {
+                    this.fields[i--] = FIlling();
+                    flag = true;
+                }
+                else if (int.TryParse(label, out int result)  && this.fields[i].Age == result)
+                {
+                    this.fields[i--] = FIlling();
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+        public void Sort(string select)
+        {
+            Field temp;
+            bool flag = true;
+
+            switch (select)
+            {
+                case "1":
+                    while (flag)
+                    {
+                        flag = false;
+                        for (int i = 0; i < this.index - 1; ++i)
+                            if (this.fields[i].FirstName.CompareTo(this.fields[i + 1].FirstName) > 0)
+                            {
+                                temp = this.fields[i];
+                                this.fields[i] = this.fields[i + 1];
+                                this.fields[i + 1] = temp;
+                                flag = true;
+                            }
+                    }
+                    break;
+                case "2":
+                    while (flag)
+                    {
+                        flag = false;
+                        for (int i = 0; i < this.index - 1; ++i)
+                            if (this.fields[i].LastName.CompareTo(this.fields[i + 1].LastName) > 0)
+                            {
+                                temp = this.fields[i];
+                                this.fields[i] = this.fields[i + 1];
+                                this.fields[i + 1] = temp;
+                                flag = true;
+                            }
+                    }
+
+                    break;
+                case "3":
+                    for (int i = 0; i < this.index - 1; i++)
+                    {
+                        for (int j = i + 1; j < this.index; j++)
+                        {
+                            if (this.fields[i].Age > this.fields[j].Age)
+                            {
+                                temp = this.fields[i];
+                                this.fields[i] = this.fields[j];
+                                this.fields[j] = temp;
+                            }
+                        };
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Такой команды нет...");
+                    break;
+
+            }
+        }
+        /// <summary>
+        /// Вывод по диапозону дат
+        /// </summary>
+        /// <param name="a">1я дата</param>
+        /// <param name="b">2я дата</param>
+        public void PrintSort(DateTime a,DateTime b)
+        {
+            for (int i = 0; i < this.index; i++)
+            {
+                if (this.fields[i].Date >= a && this.fields[i].Date<=b)
+                {
+                    Console.WriteLine(fields[i].Print());
+                }
+            }       
+
+        }
+        /// <summary>
+        /// Печать в консоль
+        /// </summary>
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"{this.titles[0],10}|{this.titles[1],10}|{this.titles[2],8}|{this.titles[3],8}|{this.titles[4],17}");
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(this.fields[i].Print());
+            }
+        }
+        /// <summary>
+        /// Загрузка из файла
+        /// </summary>
+        public void Load()
+        {
+            using(StreamReader sr = new StreamReader(this.path))
+            {
+                titles = sr.ReadLine().Split('|');
+                while (!sr.EndOfStream)
+                {
+                    string[] args = sr.ReadLine().Split('|');
+                    Add(new Field(args[0], args[1], int.Parse(args[2]), args[3]));
+                }
+            }
+        }
+        /// <summary>
+        /// Загрузка в файл
+        /// </summary>
+        public void UnLoad()
+        {
+            using (StreamWriter sw = new StreamWriter(this.path))
+            {
+                sw.WriteLine($"{this.titles[0],10}|{this.titles[1],10}|{this.titles[2],8}|{this.titles[3],8}|{this.titles[4],17}");
+                for (int i = 0; i < this.index; i++)
+                { 
+                    sw.WriteLine(this.fields[i].Print());
+                }              
+            }
+        }
+    }
+}
